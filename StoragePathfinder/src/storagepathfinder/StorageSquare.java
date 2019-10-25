4cadb7f55452eec015cd9fc4ccdc01a636a0d18c
schallernicolas <48554302+schallernicolas@users.noreply.github.com>
2019-10-25 10:10:07 +0200

    removed test-Method

--- StoragePathfinder/src/storagepathfinder/StorageSquare.java
+++ StoragePathfinder/src/storagepathfinder/StorageSquare.java
@@ -52,15 +52,6 @@ public class StorageSquare {
         neighbors.put(neighbor.name, price);
     }
     
-    public void test(){
-        List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
-        myList.stream()
-                .filter(s -> s.startsWith("c"))
-                .map(String::toUpperCase)
-                .sorted()
-                .forEach(System.out::println);
-    }
-    
     //only for debug purposes!
     public void printSquareInformation(){
         System.out.println("Square " + name);
